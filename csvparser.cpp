845aed072bf69f19a43a4e06bb0f803e5067325a
Patan Amrulla Khan <pamrulla@gmail.com>
2019-03-28 16:35:08 +0530

    Refractored code into manageable functions

--- csvparser.cpp
+++ csvparser.cpp
@@ -1,45 +1,64 @@
 #include "csvparser.h"
 namespace CSVparser
 {
-	csvparser::csvparser(std::istream& src) : m_src(src)
+	csvparser::csvparser(std::istream& src) : m_src(src), m_fieldSeperator(',')
 	{
+		NextChar();
 	}
 
 
 	csvparser::~csvparser()
 	{
 	}
+
 	bool csvparser::NextRow(CSVRow & row)
 	{
 		row.clear();
-		char nextCh;
-		std::string field;
-		while (m_src.get(nextCh))
+		while (!IsRecordEnd())
 		{
-			if (nextCh == ',')
-			{
-				row.push_back(field);
-				field = "";
-			}
-			else if(nextCh == '\n')
-			{
-				row.push_back(field);
-				field = "";
-				break;
-			}
-			else if (nextCh == '\r')
-			{
-				continue;
-			}
-			else
-			{
-				field += nextCh;
-			}
-		}
-		if (!field.empty())
-		{
-			row.push_back(field);
+			ReadField(row);
 		}
 		return row.size() != 0;
+	}
+	bool csvparser::IsFieldEnd()
+	{
+		return m_nextCh == m_fieldSeperator || m_nextCh == REC_SEP || m_nextCh == END_INPUT;
+	}
+	bool csvparser::IsRecordEnd()
+	{
+		if (m_nextCh == END_INPUT || m_nextCh == REC_SEP)
+		{
+			NextChar();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	void csvparser::NextChar()
+	{
+		if (!m_src.get(m_nextCh))
+		{
+			m_nextCh = END_INPUT;
+		}
+		if (m_nextCh == '\r')
+		{
+			NextChar();
+		}
+	}
+	void csvparser::ReadField(CSVRow & row)
+	{
+		std::string field;
+		while (!IsFieldEnd())
+		{
+			field += m_nextCh;
+			NextChar();
+		}
+		row.push_back(field);
+		if (m_nextCh == m_fieldSeperator)
+		{
+			NextChar();
+		}
 	}
 }
--- csvparser.h
+++ csvparser.h
@@ -6,6 +6,8 @@
 namespace CSVparser
 {
 	typedef std::vector<std::string> CSVRow;
+	const char END_INPUT = 0;
+	const char REC_SEP = '\n';
 
 	class csvparser
 	{
@@ -16,6 +18,13 @@ namespace CSVparser
 		bool NextRow(CSVRow& row);
 
 	private:
+		bool IsFieldEnd();
+		bool IsRecordEnd();
+		void NextChar();
+		void ReadField(CSVRow& row);
+
 		std::istream& m_src;
+		char m_fieldSeperator;
+		char m_nextCh;
 	};
 }
